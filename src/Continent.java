d57b3488a3ca756bd204fbb4024677ccfeca0423
Leandro Silveira <33546260+Silveiraleandro@users.noreply.github.com>
2020-03-09 18:04:38 +0000

    new classes added User, Country and Enum
    the classes were added to the project, but they are still not useful

---
+++ src/Continent.java
@@ -0,0 +1,10 @@
+public enum Continent {
+//Enum class for Continent since the continents won't ever change
+    Europe,
+    Asia,
+    NorthAmerica,
+    SouthAmerica,
+    Africa,
+    Oceania,
+    Antarctica
+}
---
+++ src/Country.java
@@ -0,0 +1,13 @@
+public class Country {
+
+    private Country name;
+    private Country code;
+
+    public Country(){
+
+
+    }
+    public void addCountry(){
+
+    }
+}
